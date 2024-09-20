55409555ccc31fbc4380744af38f05b2ac5175a9
Emil Pradeep <emilpradeep@gmail.com>
2024-09-20 10:51:46 +0530

    removed unused importa

--- src/main/java/com/xperia/xpense_tracker/config/SecurityConfig.java
+++ src/main/java/com/xperia/xpense_tracker/config/SecurityConfig.java
@@ -1,13 +1,11 @@
 package com.xperia.xpense_tracker.config;
 
 import com.xperia.xpense_tracker.services.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
