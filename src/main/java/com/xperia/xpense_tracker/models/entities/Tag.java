c4301aa7f577845e4162064bc0d23071a7d73030
Emil Pradeep <emilpradeep@gmail.com>
2024-11-30 19:48:10 +0530

    implemented update expense api

--- src/main/java/com/xperia/xpense_tracker/models/entities/Tag.java
+++ src/main/java/com/xperia/xpense_tracker/models/entities/Tag.java
@@ -2,7 +2,6 @@ package com.xperia.xpense_tracker.models.entities;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
