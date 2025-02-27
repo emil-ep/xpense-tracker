c83a3223743822a5de45ab511c744dbf561413be
Emil Pradeep <emilpradeep@gmail.com>
2025-02-27 17:08:08 +0530

    updated data initializer

--- src/main/java/com/xperia/xpense_tracker/initializer/DataInitializer.java
+++ src/main/java/com/xperia/xpense_tracker/initializer/DataInitializer.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -28,11 +30,15 @@ public class DataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         List<TagCategory> tagCategories =  tagCategoryRepository.findAll();
-        if (tagCategories.isEmpty()){
-            LOGGER.debug("Tag categories are empty. Populating from list...");
+        if (tagCategories.isEmpty() || tagCategories.size() != TagCategoryEnum.values().length){
+
+            Set<String> existingTagCategoryNames = tagCategories.stream().map(TagCategory::getName).collect(Collectors.toSet());
+            LOGGER.debug("Tag categories are empty or new tag categories are added. Populating from list...");
             List<TagCategory> list = Arrays.stream(TagCategoryEnum.values())
+                    .filter(tagCategoryEnum -> !existingTagCategoryNames.contains(tagCategoryEnum.getName()))
                     .map(tagCategoryEnum -> new TagCategory(tagCategoryEnum.getName(), tagCategoryEnum.getIsExpense()))
                     .toList();
+            LOGGER.debug("{} tag categories are added", list.size());
             tagCategoryRepository.saveAll(list);
             LOGGER.debug("Tag categories populated");
         }
