869a68a6481834fe85fde854f870441cbeb93556
Emil <emilpradeep@gmail.com>
2026-01-08 18:32:09 +0530

    Created a controller for handling mcp request. This is a base controller and would change in future

---
+++ src/main/java/com/xperia/xpense_tracker/mcp/McpController.java
@@ -0,0 +1,35 @@
+package com.xperia.xpense_tracker.mcp;
+
+import com.xperia.xpense_tracker.mcp.models.McpRequest;
+import com.xperia.xpense_tracker.mcp.models.McpResponse;
+import com.xperia.xpense_tracker.mcp.tool.McpTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/mcp")
+public class McpController {
+
+    @Autowired
+    private McpToolRegistry toolRegistry;
+
+    @PostMapping
+    public McpResponse<?> execute(@RequestBody McpRequest request){
+
+        McpTool tool = toolRegistry.getTool(request.getTool());
+
+        if (tool == null){
+            return McpResponse.error("Unknown tool : " + request.getTool());
+        }
+
+        try{
+            Object result = tool.execute(request.getArguments());
+            return McpResponse.success(result);
+        }catch (Exception ex){
+            return McpResponse.error(ex.getMessage());
+        }
+    }
+}
