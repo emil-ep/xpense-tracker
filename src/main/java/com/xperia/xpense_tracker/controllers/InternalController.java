6c0daffff46b0ed789fa73e110d1a5ab2fa3ec5a
Emil <emilpradeep@gmail.com>
2026-09-12 20:14:19 +0530

    feat(internal): created endpoint to handle user setting sharing

--- src/main/java/com/xperia/xpense_tracker/controllers/InternalController.java
+++ src/main/java/com/xperia/xpense_tracker/controllers/InternalController.java
@@ -1,12 +1,16 @@
 package com.xperia.xpense_tracker.controllers;
 
 import com.xperia.xpense_tracker.models.response.AbstractResponse;
+import com.xperia.xpense_tracker.models.response.ErrorResponse;
 import com.xperia.xpense_tracker.models.response.SuccessResponse;
 import com.xperia.xpense_tracker.services.InternalService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.xperia.exception.TrackerNotFoundException;
+import org.xperia.models.SharedUserSetting;
 import org.xperia.models.UserOauthToken;
 
 import java.net.URLDecoder;
@@ -17,6 +21,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/internal")
 public class InternalController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InternalController.class);
 
     private final InternalService internalService;
 
@@ -29,6 +35,20 @@ public class InternalController {
     public ResponseEntity<AbstractResponse> fetchGoogleDetailsOfUsers(){
         List<UserOauthToken> validTokens = this.internalService.findUsersWithGoogleAccessToken();
         return ResponseEntity.ok().body(new SuccessResponse(validTokens));
+    }
+
+    @GetMapping("/user/settings")
+    public ResponseEntity<AbstractResponse> fetchUserSettings(@RequestParam("email") String email,
+                                                              @RequestParam("settingsType") String type){
+
+        try{
+            email = URLDecoder.decode(email, StandardCharsets.UTF_8);
+            SharedUserSetting userSettings = this.internalService.findUserSettingsByType(email, type);
+            return ResponseEntity.ok(new SuccessResponse(userSettings));
+        }catch (Exception ex){
+            LOGGER.error("Error fetching settings for user {}", ex.getMessage());
+            return ResponseEntity.internalServerError().body(new ErrorResponse(ex.getMessage()));
+        }
     }
 
     @PostMapping(value = "/refresh/token", produces = "application/json")
