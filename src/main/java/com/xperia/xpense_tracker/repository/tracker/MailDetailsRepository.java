2d43080d19cb45e02c03ed5042ba7cd4cc689b09
Emil <emilpradeep@gmail.com>
2026-09-14 22:39:58 +0530

    impl(mail_details): implemented mail_details service

--- src/main/java/com/xperia/xpense_tracker/repository/tracker/MailDetailsRepository.java
+++ src/main/java/com/xperia/xpense_tracker/repository/tracker/MailDetailsRepository.java
@@ -1,9 +1,14 @@
 package com.xperia.xpense_tracker.repository.tracker;
 
 import com.xperia.xpense_tracker.models.entities.tracker.MailDetails;
+import com.xperia.xpense_tracker.models.entities.tracker.TrackerUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MailDetailsRepository extends JpaRepository<MailDetails, String> {
+
+    Optional<MailDetails> findMailDetailsByUser(TrackerUser user);
 }
--- src/main/java/com/xperia/xpense_tracker/services/impl/MailDetailsServiceImpl.java
+++ src/main/java/com/xperia/xpense_tracker/services/impl/MailDetailsServiceImpl.java
@@ -1,8 +1,35 @@
 package com.xperia.xpense_tracker.services.impl;
 
+import com.xperia.xpense_tracker.models.entities.tracker.MailDetails;
+import com.xperia.xpense_tracker.models.entities.tracker.TrackerUser;
+import com.xperia.xpense_tracker.repository.tracker.MailDetailsRepository;
 import com.xperia.xpense_tracker.services.MailDetailsService;
+import com.xperia.xpense_tracker.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.xperia.exception.TrackerBadRequestException;
+
+import java.util.Optional;
 
 @Service
 public class MailDetailsServiceImpl implements MailDetailsService {
+
+    private final UserService userService;
+
+    private final MailDetailsRepository mailDetailsRepository;
+
+    @Autowired
+    public MailDetailsServiceImpl(UserService userService, MailDetailsRepository mailDetailsRepository){
+        this.userService = userService;
+        this.mailDetailsRepository = mailDetailsRepository;
+    }
+
+    @Override
+    public Optional<MailDetails> findMailDetailsByUserId(String userId) {
+        Optional<TrackerUser> user = this.userService.findUserByUserId(userId);
+        if (user.isEmpty()){
+            throw new TrackerBadRequestException("User with id : " + userId + " not found");
+        }
+        return mailDetailsRepository.findMailDetailsByUser(user.get());
+    }
 }
